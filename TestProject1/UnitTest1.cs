87a2653590da94bb68a43f31215cdea375c1b6d5
victoriachappuis <svchappuis@gmail.com>
2024-05-14 09:16:07 -0300

    add BaldosaCuadrada

--- TestProject1/UnitTest1.cs
+++ TestProject1/UnitTest1.cs
@@ -7,11 +7,20 @@ public class UnitTest1
 {
     private string _nombreMaterial = "ceramica";
     private int _precioMaterial = 10;
+    
     [TestMethod]
     public void Material_ShouldConstruct()
     {
         var material = new Material(_nombreMaterial, _precioMaterial);
         Assert.AreEqual(_nombreMaterial, material.Nombre);
         Assert.AreEqual(_precioMaterial, material.Precio);
+    }
+    
+    [TestMethod]
+    public void BaldosaCuadrada_ShouldCalculateArea()
+    {
+        var lado = 5;
+        var baldosa = new BaldosaCuadrada { Lado = lado };
+        Assert.AreEqual(lado * lado, baldosa.Area());
     }
 }
--- baldosas/Program.cs
+++ baldosas/Program.cs
@@ -13,6 +13,26 @@ public class Material
     }
 }
 
+public abstract class Baldosa
+{
+    public Material Material { get; set; }
+    public abstract int Area();
+
+    public int Precio()
+    {
+        return this.Area() * this.Material.Precio;
+    }
+}
+
+public class BaldosaCuadrada : Baldosa
+{
+    public int Lado { get; set; }
+    public override int Area()
+    {
+        return Lado * Lado;
+    }
+}
+
         public class Program
         {
             public static void Main(string[] args)
