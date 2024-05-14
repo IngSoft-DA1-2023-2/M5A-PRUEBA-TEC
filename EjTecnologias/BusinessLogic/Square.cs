c2c0259ad93e24eb650a4143f29aad374a305545
Alejo Fraga <af281542@fi365.ort.edu.uy>
2024-05-14 08:57:27 -0300

    [GREEN]: ShouldSetMaterial

--- EjTecnologias/BusinessLogic/Square.cs
+++ EjTecnologias/BusinessLogic/Square.cs
@@ -3,9 +3,12 @@ namespace BusinessLogic;
 public class Square : Shape
 {
     public int Side { set; get; }
-    
+    public string Material { set; get; }
+
     public int Area()
     {
         return Side * Side;
     }
+    
+    
 }
