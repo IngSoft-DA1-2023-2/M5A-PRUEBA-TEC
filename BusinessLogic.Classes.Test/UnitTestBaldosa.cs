3f0a20444d0a9076987ccca3e56f0afc57601153
Mauro Correa <MC287417@fi365.ort.edu.uy>
2024-05-14 08:23:02 -0300

    Creo proyecto y agrego clase abstracta baldosa

---
+++ BusinessLogic.Classes.Test/UnitTestBaldosa.cs
@@ -0,0 +1,10 @@
+namespace BusinessLogic.Classes.Test;
+
+[TestClass]
+public class UnitTestBaldosa
+{
+    [TestMethod]
+    public void TestAnchoProperty()
+    {   
+    }
+}
---
+++ BusinessLogic.Classes/Baldosa.cs
@@ -0,0 +1,15 @@
+﻿namespace BusinessLogic.Classes;
+
+public abstract class Baldosa
+{
+    public Baldosa(int largo, int ancho){
+        Largo = largo;
+        Ancho = ancho;
+    }
+    public int Largo { get; set; }
+    private int Ancho {get; set;}
+
+    public abstract int CalcularArea();
+    public abstract int CalcularPrecio();
+
+}
