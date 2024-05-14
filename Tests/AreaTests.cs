3ea0b22ffe7e17024b8726e04e6e9cef57fae20b
Agustín Madrid <agustin.madrid@quanam.com>
2024-05-14 08:23:19 -0300

    [RED] Square Calculate

--- Tests/AreaTests.cs
+++ Tests/AreaTests.cs
@@ -11,4 +11,11 @@ public class AreaTests
         Area area = new Area();
         Assert.IsNotNull(area);
     }
+    
+    public void SquareAreaCalculate()
+    {
+        Area area = new Area();
+        double result = area.SquareAreaCalculate(5);
+        Assert.AreEqual(25, result);
+    }
 }
