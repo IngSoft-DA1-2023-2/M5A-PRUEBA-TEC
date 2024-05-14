32601250a7aebfd8567769c76578ec5c0258347a
Alejo Fraga <af281542@fi365.ort.edu.uy>
2024-05-14 08:50:51 -0300

    [RED]: ShouldSetSquareSide

--- EjTecnologias/BusinessLogic.Test/Square.Test.cs
+++ EjTecnologias/BusinessLogic.Test/Square.Test.cs
@@ -11,4 +11,13 @@ public class Square_Test
         var square = new Square();
         Assert.IsNotNull(square);
     }
+    
+    
+    [TestMethod]
+    public void ShouldSetSquareSide()
+    {
+        var square = new Square();
+        square.Side = 5;
+        Assert.AreEqual(5, square.Side);
+    }
 }
